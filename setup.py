0824a153e77bc0900d7167f576df5dbd8dbbd34d
Kefkius <kefkius@mail.com>
2015-02-14 16:42:08 -0500

    FRC - setup, icon name

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ version = imp.load_source('version', 'lib/version.py')
 util = imp.load_source('util', 'lib/util.py')
 
 if sys.version_info[:3] < (2, 7, 0):
-    sys.exit("Error: Electrum requires Python version >= 2.7.0...")
+    sys.exit("Error: Electrum-frc requires Python version >= 2.7.0...")
 
 
 
@@ -26,11 +26,11 @@ if (len(sys.argv) > 1) and (sys.argv[1] == "install"):
         try:
             os.mkdir(usr_share)
         except:
-            sys.exit("Error: cannot write to %s.\nIf you do not have root permissions, you may install Electrum in a virtualenv.\nAlso, please note that you can run Electrum without installing it on your system."%usr_share)
+            sys.exit("Error: cannot write to %s.\nIf you do not have root permissions, you may install Electrum-frc in a virtualenv.\nAlso, please note that you can run Electrum-frc without installing it on your system."%usr_share)
 
     data_files += [
-        (os.path.join(usr_share, 'applications/'), ['electrum.desktop']),
-        (os.path.join(usr_share, 'app-install', 'icons/'), ['icons/electrum.png'])
+        (os.path.join(usr_share, 'applications/'), ['electrum-frc.desktop']),
+        (os.path.join(usr_share, 'app-install', 'icons/'), ['icons/electrum-frc.png'])
     ]
     if not os.path.exists('locale'):
         os.mkdir('locale')
@@ -39,7 +39,7 @@ if (len(sys.argv) > 1) and (sys.argv[1] == "install"):
             data_files.append((os.path.join(usr_share, 'locale/%s/LC_MESSAGES' % lang), ['locale/%s/LC_MESSAGES/electrum.mo' % lang]))
 
 
-    appdata_dir = os.path.join(usr_share, "electrum")
+    appdata_dir = os.path.join(usr_share, "electrum-frc")
     data_files += [
         (appdata_dir, ["data/README"]),
         (os.path.join(appdata_dir, "cleanlook"), [
@@ -62,7 +62,7 @@ else:
     data_files = []
 
 setup(
-    name="Electrum",
+    name="Electrum-frc",
     version=version.ELECTRUM_VERSION,
     install_requires=[
         'slowaes>=0.1a1',
@@ -77,75 +77,75 @@ setup(
         'tlslite'
     ],
     package_dir={
-        'electrum': 'lib',
-        'electrum_gui': 'gui',
-        'electrum_plugins': 'plugins',
+        'electrum_frc': 'lib',
+        'electrum_frc_gui': 'gui',
+        'electrum_frc_plugins': 'plugins',
     },
-    scripts=['electrum'],
+    scripts=['electrum-frc'],
     data_files=data_files,
     py_modules=[
-        'electrum.account',
-        'electrum.bitcoin',
-        'electrum.blockchain',
-        'electrum.bmp',
-        'electrum.commands',
-        'electrum.daemon',
-        'electrum.i18n',
-        'electrum.interface',
-        'electrum.mnemonic',
-        'electrum.msqr',
-        'electrum.network',
-        'electrum.network_proxy',
-        'electrum.old_mnemonic',
-        'electrum.paymentrequest',
-        'electrum.paymentrequest_pb2',
-        'electrum.plugins',
-        'electrum.qrscanner',
-        'electrum.simple_config',
-        'electrum.synchronizer',
-        'electrum.transaction',
-        'electrum.util',
-        'electrum.verifier',
-        'electrum.version',
-        'electrum.wallet',
-        'electrum.x509',
-        'electrum_gui.gtk',
-        'electrum_gui.qt.__init__',
-        'electrum_gui.qt.amountedit',
-        'electrum_gui.qt.console',
-        'electrum_gui.qt.history_widget',
-        'electrum_gui.qt.icons_rc',
-        'electrum_gui.qt.installwizard',
-        'electrum_gui.qt.lite_window',
-        'electrum_gui.qt.main_window',
-        'electrum_gui.qt.network_dialog',
-        'electrum_gui.qt.password_dialog',
-        'electrum_gui.qt.paytoedit',
-        'electrum_gui.qt.qrcodewidget',
-        'electrum_gui.qt.qrtextedit',
-        'electrum_gui.qt.qrwindow',
-        'electrum_gui.qt.receiving_widget',
-        'electrum_gui.qt.seed_dialog',
-        'electrum_gui.qt.transaction_dialog',
-        'electrum_gui.qt.util',
-        'electrum_gui.qt.version_getter',
-        'electrum_gui.stdio',
-        'electrum_gui.text',
-        'electrum_plugins.btchipwallet',
-        'electrum_plugins.coinbase_buyback',
-        'electrum_plugins.cosigner_pool',
-        'electrum_plugins.exchange_rate',
-        'electrum_plugins.greenaddress_instant',
-        'electrum_plugins.labels',
-        'electrum_plugins.trezor',
-        'electrum_plugins.virtualkeyboard',
-        'electrum_plugins.plot',
+        'electrum_frc.account',
+        'electrum_frc.bitcoin',
+        'electrum_frc.blockchain',
+        'electrum_frc.bmp',
+        'electrum_frc.commands',
+        'electrum_frc.daemon',
+        'electrum_frc.i18n',
+        'electrum_frc.interface',
+        'electrum_frc.mnemonic',
+        'electrum_frc.msqr',
+        'electrum_frc.network',
+        'electrum_frc.network_proxy',
+        'electrum_frc.old_mnemonic',
+        'electrum_frc.paymentrequest',
+        'electrum_frc.paymentrequest_pb2',
+        'electrum_frc.plugins',
+        'electrum_frc.qrscanner',
+        'electrum_frc.simple_config',
+        'electrum_frc.synchronizer',
+        'electrum_frc.transaction',
+        'electrum_frc.util',
+        'electrum_frc.verifier',
+        'electrum_frc.version',
+        'electrum_frc.wallet',
+        'electrum_frc.x509',
+        'electrum_frc_gui.gtk',
+        'electrum_frc_gui.qt.__init__',
+        'electrum_frc_gui.qt.amountedit',
+        'electrum_frc_gui.qt.console',
+        'electrum_frc_gui.qt.history_widget',
+        'electrum_frc_gui.qt.icons_rc',
+        'electrum_frc_gui.qt.installwizard',
+        'electrum_frc_gui.qt.lite_window',
+        'electrum_frc_gui.qt.main_window',
+        'electrum_frc_gui.qt.network_dialog',
+        'electrum_frc_gui.qt.password_dialog',
+        'electrum_frc_gui.qt.paytoedit',
+        'electrum_frc_gui.qt.qrcodewidget',
+        'electrum_frc_gui.qt.qrtextedit',
+        'electrum_frc_gui.qt.qrwindow',
+        'electrum_frc_gui.qt.receiving_widget',
+        'electrum_frc_gui.qt.seed_dialog',
+        'electrum_frc_gui.qt.transaction_dialog',
+        'electrum_frc_gui.qt.util',
+        'electrum_frc_gui.qt.version_getter',
+        'electrum_frc_gui.stdio',
+        'electrum_frc_gui.text',
+        'electrum_frc_plugins.btchipwallet',
+        'electrum_frc_plugins.coinbase_buyback',
+        'electrum_frc_plugins.cosigner_pool',
+        'electrum_frc_plugins.exchange_rate',
+        'electrum_frc_plugins.greenaddress_instant',
+        'electrum_frc_plugins.labels',
+        'electrum_frc_plugins.trezor',
+        'electrum_frc_plugins.virtualkeyboard',
+        'electrum_frc_plugins.plot',
 
     ],
-    description="Lightweight Bitcoin Wallet",
+    description="Lightweight Freicoin Wallet",
     author="Thomas Voegtlin",
     author_email="thomasv1@gmx.de",
     license="GNU GPLv3",
     url="https://electrum.org",
-    long_description="""Lightweight Bitcoin Wallet"""
+    long_description="""Lightweight Freicoin Wallet"""
 )
